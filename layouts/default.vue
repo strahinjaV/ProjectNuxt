416613f36b5d15e9f546921a8bdf9294a3fb3ddf
Strahinja <strahinjav98@gmail.com>
2023-01-19 23:11:06 +0100

    lesson 4 completed, works as it should

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,10 +9,21 @@
     import NavBar from '~/components/NavBar.vue'
 
     export default {
-        components:{
+      components:{
             NavBar
+        },
+
+        head () {
+          return {
+            titleTemplate: '%s - Real World Events',
+              meta: [{
+                hid: 'description',
+                name: 'description',
+                content: 'Where you can find all the events taking place in your neighborhood'
+              }]
+            }
         }
-    }
+     }
   </script>
   
   <style>
--- pages/create.vue
+++ pages/create.vue
@@ -3,3 +3,19 @@
       <h1>Create an Event</h1>
     </div>
   </template>
+
+<script>
+  
+export default {
+  head () {
+    return {
+      title: 'Create an Event',
+      meta: [{
+        hid: 'description',
+        name: 'description',
+        content: 'You can create an event in your neighborhood'
+      }]
+    }
+  }
+}
+</script>
