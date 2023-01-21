22ea7defa200700c70abd5979fabc5492fa1ff09
Strahinja <strahinjav98@gmail.com>
2023-01-21 17:13:49 +0100

    pushing changes from lesson8 to main

--- services/EventService.js
+++ services/EventService.js
@@ -1,19 +1,19 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-    baseURL: 'http://localhost:3000',
-    withCredentials: false,
-    headers: {
-        Accept: 'application/json',
-        'ContentType': 'application/json'
-    }
+  baseURL: 'https://my-json-server.typicode.com/strahinjaV/ProjectNuxt',
+  withCredentials: false,
+  headers: {
+    Accept: 'application/json',
+    ContentType: 'application/json',
+  },
 })
 
 export default {
-    getEvents() {
-        return apiClient.get('/events')
-    },
-    getEvent(id){
-        return apiClient.get('/events/' + id)
-    }
+  getEvents() {
+    return apiClient.get('/events')
+  },
+  getEvent(id) {
+    return apiClient.get('/events/' + id)
+  },
 }
